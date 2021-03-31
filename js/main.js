efa85ace6110eed08fc44dda08875711d68ddd29
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-03-31 23:35:22 +0200

    fixed months names

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const apiMonth31 = [
   "Ante Diem III Kalendas",
   "Pridie Kalendas",
 ];
-const apiMonth29 = [
+const apiMonth31Bis = [
   "Kalendas",
   "Ante Diem IV Nonas",
   "Ante Diem III Nonas",
@@ -47,6 +47,40 @@ const apiMonth29 = [
   "Ante Diem III Idus",
   "Pridie Idus",
   "IDUS",
+  "Ante Diem XIX Kalendas",
+  "Ante Diem XVIII Kalendas",
+  "Ante Diem XVII Kalendas",
+  "Ante Diem XVI Kalendas",
+  "Ante Diem XV Kalendas",
+  "Ante Diem XIV Kalendas",
+  "Ante Diem XIII Kalendas",
+  "Ante Diem XII Kalendas",
+  "Ante Diem XI Kalendas",
+  "Ante Diem X Kalendas",
+  "Ante Diem IX Kalendas",
+  "Ante Diem VIII Kalendas",
+  "Ante Diem VII Kalendas",
+  "Ante Diem VI Kalendas",
+  "Ante Diem V Kalendas",
+  "Ante Diem IV Kalendas",
+  "Ante Diem III Kalendas",
+  "Pridie Kalendas",
+];
+const apiMonth30 = [
+  "Kalendas",
+  "Ante Diem IV Nonas",
+  "Ante Diem III Nonas",
+  "Pridie Nonas",
+  "NONAS",
+  "Ante Diem VIII Idus",
+  "Ante Diem VII Idus",
+  "Ante Diem VI Idus",
+  "Ante Diem V Idus",
+  "Ante Diem IV Idus",
+  "Ante Diem III Idus",
+  "Pridie Idus",
+  "IDUS",
+  "Ante Diem XVIII Kalendas",
   "Ante Diem XVII Kalendas",
   "Ante Diem XVI Kalendas",
   "Ante Diem XV Kalendas",
@@ -65,34 +99,35 @@ const apiMonth29 = [
   "Pridie Kalendas",
 ];
 const apiMonth28 = [
-  "Kalendas",
-  "Ante Diem IV Nonas",
-  "Ante Diem III Nonas",
-  "Pridie Nonas",
-  "NONAS",
-  "Ante Diem VIII Idus",
-  "Ante Diem VII Idus",
-  "Ante Diem VI Idus",
-  "Ante Diem V Idus",
-  "Ante Diem IV Idus",
-  "Ante Diem III Idus",
-  "Pridie Idus",
-  "IDUS",
-  "Ante Diem XVI Kalendas",
-  "Ante Diem XV Kalendas",
-  "Ante Diem XIV Kalendas",
-  "Ante Diem XIII Kalendas",
-  "Ante Diem XII Kalendas",
-  "Ante Diem XI Kalendas",
-  "Ante Diem X Kalendas",
-  "Ante Diem IX Kalendas",
-  "Ante Diem VIII Kalendas",
-  "Ante Diem VII Kalendas",
-  "Ante Diem VI Kalendas",
-  "Ante Diem V Kalendas",
-  "Ante Diem IV Kalendas",
-  "Ante Diem III Kalendas",
-  "Pridie Kalendas",
+  "Kalendas Februarias",
+  "Ante Diem IV Nonas Februarias",
+  "Ante Diem III Nonas Februarias",
+  "Pridie Nonas Februarias",
+  "NONAS Februarias",
+  "Ante Diem VIII Idus Februarias",
+  "Ante Diem VII Idus Februarias",
+  "Ante Diem VI Idus Februarias",
+  "Ante Diem V Idus Februarias",
+  "Ante Diem IV Idus Februarias",
+  "Ante Diem III Idus Februarias",
+  "Pridie Idus Februarias",
+  "IDUS Februarias",
+  "Ante Diem XVI Kalendas Martias",
+  "Ante Diem XV Kalendas Martias",
+  "Ante Diem XIV Kalendas Martias",
+  "Ante Diem XIII Kalendas Martias",
+  "Ante Diem XII Kalendas Martias",
+  "Ante Diem XI Kalendas Martias",
+  "Ante Diem X Kalendas Martias",
+  "Ante Diem IX Kalendas Martias",
+  "Ante Diem VIII Kalendas Martias",
+  "Ante Diem VII Kalendas Martias",
+  "Ante Diem VI Kalendas Martias",
+  "Ante Diem V Kalendas Martias",
+  "Ante Diem IV Kalendas Martias",
+  "Ante Diem III Kalendas Martias",
+  "Pridie Kalendas Martias",
+  "Bis VI ante kalendas Martias",
 ];
 const dayValue = document.querySelector(".js-inputDay");
 const monthValue = document.querySelector(".js-inputMonth");
@@ -102,87 +137,89 @@ const date = document.querySelector(".js-text");
 let day = "";
 let month = "";
 let htmlCode = "";
-
+const latMonths = [
+  "Ianuarias",
+  "Februarias",
+  "Martias",
+  "Apriles",
+  "Maias",
+  "Iunias",
+  "Quintiles",
+  "Sextiles",
+  "Septembres",
+  "Octobres",
+  "Novembres",
+  "Decembres",
+];
+const spaMonths = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+];
 const getInputValue = (ev) => {
   ev.preventDefault();
   day = dayValue.value;
   month = monthValue.value;
-  condicionalName();
+  filterMonth();
+};
+const filterMonth = () => {
+  const indexMonth = spaMonths.findIndex(
+    (spaMonth) => spaMonth === month.toLowerCase()
+  );
+  month = indexMonth;
+  getDay();
 };
 
-const condicionalName = () => {
-  switch (month.toLowerCase()) {
-    case "enero":
-      month = "Ianuarias";
-      getDay29();
-      break;
-    case "febrero":
-      month = "Februarias";
-      getDay29();
-      break;
-    case "marzo":
-      month = "Martias";
-      getDay31();
-      break;
-    case "abril":
-      month = "Apriles";
-      getDay29();
-      break;
-    case "mayo":
-      month = "Maias";
-      getDay31();
-      break;
-    case "junio":
-      month = "Iunias";
-      getDay29();
-      break;
-    case "julio":
-      month = "Quintiles";
-      getDay31();
-      break;
-    case "agosto":
-      month = "Sextiles";
-      getDay29();
-      break;
-    case "septiembre":
-      month = "Septembres";
-      getDay29();
-      break;
-    case "octubre":
-      month = "Octobres";
-      getDay31();
-      break;
-    case "noviembre":
-      month = "Novembres";
-      getDay29();
-      break;
-    case "diciembre":
-      month = "Decembres";
-      getDay29();
-      break;
-
-    default:
-      console.log("No existe ese mes en el calendario romano");
-      date.innerHTML = "No existe ese mes en el calendario romano";
-      break;
-  }
-};
-
-const getDay31 = () => {
+const getDay = () => {
   const dayNumber = parseInt(day) - 1;
-  if (dayNumber <= 30) {
-    date.innerHTML = `${apiMonth31[dayNumber]}` + " " + ` ${month}`;
-  } else {
-    date.innerHTML = "No existe ese día en el calendario romano";
-  }
-};
-
-const getDay29 = () => {
-  const dayNumber = parseInt(day) - 1;
-  if (month === "Februarias" && dayNumber <= 27) {
-    date.innerHTML = `${apiMonth28[dayNumber]}` + " " + ` ${month}`;
-  } else if (month !== "Februarias" && dayNumber <= 28) {
-    date.innerHTML = `${apiMonth29[dayNumber]}` + " " + ` ${month}`;
+  if (
+    dayNumber <= 14 &&
+    (month === 2 || month === 4 || month === 6 || month === 9)
+  ) {
+    date.innerHTML = `${apiMonth31[dayNumber]}` + " " + ` ${latMonths[month]}`;
+  } else if (
+    dayNumber >= 15 &&
+    dayNumber <= 30 &&
+    (month === 2 || month === 4 || month === 6 || month === 9)
+  ) {
+    const resultMonth = month + 1;
+    date.innerHTML =
+      `${apiMonth31[dayNumber]}` + " " + ` ${latMonths[resultMonth]}`;
+  } else if (dayNumber <= 12 && (month === 0 || month === 7 || month === 11)) {
+    date.innerHTML =
+      `${apiMonth31Bis[dayNumber]}` + " " + ` ${latMonths[month]}`;
+  } else if (
+    dayNumber >= 13 &&
+    dayNumber <= 30 &&
+    (month === 0 || month === 7 || month === 11)
+  ) {
+    const resultMonth = month + 1;
+    date.innerHTML =
+      `${apiMonth31Bis[dayNumber]}` + " " + ` ${latMonths[resultMonth]}`;
+  } else if (
+    dayNumber <= 12 &&
+    (month === 3 || month === 5 || month === 8 || month === 10)
+  ) {
+    date.innerHTML = `${apiMonth30[dayNumber]}` + " " + ` ${latMonths[month]}`;
+  } else if (
+    dayNumber >= 13 &&
+    dayNumber <= 29 &&
+    (month === 3 || month === 5 || month === 8 || month === 10)
+  ) {
+    const resultMonth = month + 1;
+    date.innerHTML =
+      `${apiMonth30[dayNumber]}` + " " + ` ${latMonths[resultMonth]}`;
+  } else if (month === 1 && dayNumber <= 28) {
+    date.innerHTML = apiMonth28[dayNumber];
   } else {
     date.innerHTML = "No existe ese día en el calendario romano";
   }
